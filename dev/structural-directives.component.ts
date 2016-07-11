645ce0196c08848785f12f4827e3cbc1809e2760
Matt Lunalover <mlunalover@kabbage.com>
2016-07-11 16:44:39 -0400

    added ngIf directive to structural directives component

--- dev/structural-directives.component.ts
+++ dev/structural-directives.component.ts
@@ -3,7 +3,18 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'my-structural-directives',
     template: `
-        
+        <section class="directive">
+            <h2>*ngIf</h2>
+            <div>
+                Enter a number higher than 10
+                <br>
+                <input type="text" #number (keyup)="0">
+            </div>
+            <div *ngIf="number.value > 10">
+                <h5>A heading</h5>
+                Number is greater than 10
+            </div>
+        </section>
     `
 })
 export class StructuralDirectivesComponent {
